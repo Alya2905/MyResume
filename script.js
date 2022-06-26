e84f64f90ad416b097abfc66ee0b35799881fa0d
Alya2905 <alya_007@mail.ru>
2022-06-26 17:00:18 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,72 @@
+let myVideoOne = document.getElementById('myVideoOne'); 
+myVideoOne.onmouseover = function(){
+	myVideoOne.play(); 
+};
+myVideoOne.onmouseout = function(){
+	myVideoOne.pause(); 
+};
+
+let myVideoStore=document.getElementById('myVideoTwo');
+myVideoStore.onmouseover = function(){
+	myVideoStore.play(); 
+};
+myVideoStore.onmouseout = function (){
+	myVideoStore.pause();
+};
+
+let myVideoVacation=document.getElementById('myVideoThree');
+myVideoVacation.onmouseover = function(){
+	myVideoVacation.play(); 
+};
+myVideoVacation.onmouseout = function (){
+	myVideoVacation.pause();
+};
+
+let myVideoFour=document.getElementById('myVideoFour');
+myVideoFour.onmouseover = function(){
+	myVideoFour.play(); 
+};
+myVideoFour.onmouseout = function(){
+	myVideoFour.pause(); 
+};
+
+let myVideoFive=document.getElementById('myVideoFive');
+myVideoFive.onmouseover = function(){
+	myVideoFive.play(); 
+};
+myVideoFive.onmouseout = function (){
+	myVideoFive.pause();
+};
+
+let myVideoSix=document.getElementById('myVideoSix');
+myVideoSix.onmouseover = function(){
+	myVideoSix.play(); 
+};
+myVideoSix.onmouseout = function (){
+	myVideoSix.pause();
+};
+
+let myVideoSeven=document.getElementById('myVideoSeven');
+myVideoSeven.onmouseover = function(){
+	myVideoSeven.play(); 
+};
+myVideoSeven.onmouseout = function (){
+	myVideoSeven.pause();
+};
+
+let myVideoEight=document.getElementById('myVideoEight');
+myVideoEight.onmouseover = function(){
+	myVideoEight.play(); 
+};
+myVideoEight.onmouseout = function (){
+	myVideoEight.pause();
+};
+
+let myVideoNine=document.getElementById('myVideoNine');
+myVideoNine.onmouseover = function(){
+	myVideoNine.play(); 
+};
+myVideoNine.onmouseout = function (){
+	myVideoNine.pause();
+};
+
